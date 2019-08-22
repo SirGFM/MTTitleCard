b3cc9b82146d79c31924aaed5c48f8aff1515dae
SirGFM <gfmandaji@hotmail.com>
2019-08-21 22:24:12 -0300

    srltitlecard: Retrieve user info from SRL's API

--- main.go
+++ main.go
@@ -8,14 +8,21 @@ import (
 
 func main() {
     var srlURL string
+    var srlUser string
 
     flag.StringVar(&srlURL, "srlURL", "", "URL of the player's SRL page")
+    flag.StringVar(&srlUser, "srlUser", "", "Username of player on SRL")
     flag.Parse()
 
-    if srlURL == "" {
-        panic("Missing -srlURL")
+    if srlURL == "" && srlUser == "" {
+        panic("Missing either -srlURL or -srlUser")
     }
 
-    u := srltitlecard.Get(srlURL);
-    fmt.Printf("%+v\n", u)
+    if srlURL != "" {
+        u := srltitlecard.Get(srlURL);
+        fmt.Printf("%+v\n", u)
+    } else if srlUser != "" {
+        u := srltitlecard.GetFromUsername(srlUser);
+        fmt.Printf("%+v\n", u)
+    }
 }
---
+++ srltitlecard/get_user_from_api.go
@@ -0,0 +1,80 @@
+package srltitlecard
+
+import (
+    "fmt"
+    "encoding/json"
+    "net/http"
+    "time"
+)
+
+type SrlStats struct {
+    Rank int
+    TotalRaces int
+    TotalGames int
+    FirstRace int
+    FirstRaceDate int
+    TotalTimePlayed int
+    TotalFirstPlace int
+    TotalSecondPlace int
+    TotalThirdPlace int
+    TotalQuits int
+    TotalDisqualifications int
+}
+
+type SrlPlayer struct {
+    Id int
+    Name string
+    Channel string
+    Api string
+    Twitter string
+    Youtube string
+    Country string
+}
+
+type SrlGame struct {
+     Name string
+     Abbrev string
+}
+
+type SrlApiProfile struct {
+    Stats SrlStats
+    Player SrlPlayer
+    Game SrlGame
+}
+
+func GetFromApi(url string) User {
+    // Download the user data
+    resp, err := http.Get(url)
+    if err != nil {
+        panic(fmt.Sprintf("Failed to get user from API: %+v", err))
+    }
+    defer resp.Body.Close()
+
+    dec := json.NewDecoder(resp.Body)
+
+    var api SrlApiProfile
+    err = dec.Decode(&api)
+    if err != nil {
+        panic(fmt.Sprintf("Failed to decode the JSON: %+v", err))
+    }
+
+    var u User
+
+    u.Name = api.Player.Name
+    u.FirstRace = time.Unix(int64(api.Stats.FirstRaceDate), 0).Format("Jan 2, 2006")
+    u.NumRaces = api.Stats.TotalRaces
+    dur := time.Duration(api.Stats.TotalTimePlayed)
+    u.TotalTimePlayed = (dur * time.Second).String()
+    u.NumGames = api.Stats.TotalGames
+    u.NumFirst = api.Stats.TotalFirstPlace
+    u.NumSecond = api.Stats.TotalSecondPlace
+    u.NumThird = api.Stats.TotalThirdPlace
+    u.NumForfeit = api.Stats.TotalQuits
+
+    return u
+}
+
+func GetFromUsername(username string) User {
+    url := fmt.Sprintf("http://api.speedrunslive.com/stat?player=%s", username)
+    return GetFromApi(url)
+}
